9582888fa8373bda0719bbc4451ee725cfe2f82d
thing-a-verse <39748470+thing-a-verse@users.noreply.github.com>
2021-12-22 15:30:47 +1100

    Update RogueJndi.java

--- src/main/java/artsploit/RogueJndi.java
+++ src/main/java/artsploit/RogueJndi.java
@@ -1,13 +1,17 @@
 package artsploit;
 
+import java.sql.Timestamp;
+
 public class RogueJndi {
 
     public static void main(String[] args) throws Exception {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         System.out.println(
             "+-+-+-+-+-+-+-+-+-+\n" +
             "|R|o|g|u|e|J|n|d|i|\n" +
             "+-+-+-+-+-+-+-+-+-+"
         );
+        System.out.println(timestamp + "Restart");
         Config.applyCmdArgs(args);
         HttpServer.start();
         LdapServer.start();
